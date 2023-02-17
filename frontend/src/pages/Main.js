7f30f078dfe13a6f69e2bf77a64604629a3593ec
Vaani <vaanipathariya@gmail.com>
2023-02-17 23:00:04 +0530

    signup-signin page

--- frontend/src/pages/Main.js
+++ frontend/src/pages/Main.js
@@ -1,6 +1,16 @@
+import "./main.css"
 const Main=()=>{
     return (
-        <div>hello after loading is complete</div>
+        <div className="wrapper-main">
+            <div className="signup">
+                <h1>New here ?<br></br>lets signup first</h1><br></br>
+                <button><h2>Signup</h2></button>
+            </div>
+            <div className="signin">
+                <h1>Here to chill again</h1><br></br>
+                <button><h2>Signin</h2></button>
+            </div>
+        </div>
     )
 }
 export default Main;
