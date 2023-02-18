6b1a00e4b63691eab48458c0a7b3bd9754eb178e
Vaani <vaanipathariya@gmail.com>
2023-02-18 05:58:48 +0530

    adding backend template

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,20 @@
 const express=require('express')
 const bodyParser=require('body-parser')
 const cors=require('cors')
+const mongoose=require('mongoose')
 const app=express();
 const port = 3001;
 app.use(cors());
-app.use(bodyParser.urlencoded({extended:false}));
-app.use(bodyParser.json());
-app.listen(port,()=>console.log(`hello world listening on port: ${port}`))
+mongoose.connect("mongodb+srv://vaani:vaani@cluster0.v6vzm0u.mongodb.net/?retryWrites=true&w=majority",{
+    useNewUrlParser:true,
+    useUnifiedTopology:true
+});
+const db=mongoose.connection;
+db.on("error",console.error.bind(console,"connection error:"));
+db.once("open",function(){
+    console.log("Connected Successfully");
+})
+app.use (bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json()); 
+
+app.listen(port,()=>console.log(`Hello world listening from port ${port}!`))
